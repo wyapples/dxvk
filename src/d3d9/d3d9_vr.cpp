1490fbca62043d500676418750be44e5800fc141
TheIronWolfModding <vleonavicius@hotmail.com>
2023-01-08 13:31:26 -0800

    [vr] Port VR interface improvements from the main branch.

--- src/d3d9/d3d9_vr.cpp
+++ src/d3d9/d3d9_vr.cpp
@@ -9,122 +9,129 @@
 
 namespace dxvk {
 
-  class D3D9VR final : public ComObjectClamp<IDirect3DVR9>
+class D3D9VR final : public ComObjectClamp<IDirect3DVR9>
+{
+public:
+  D3D9VR(IDirect3DDevice9* pDevice)
+    : m_device(static_cast<D3D9DeviceEx*>(pDevice))
+  {}
+
+  HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppvObject)
   {
-  public:
+    if (ppvObject == nullptr)
+      return E_POINTER;
 
-    D3D9VR(IDirect3DDevice9* pDevice)
-      : m_device(static_cast<D3D9DeviceEx*>(pDevice))
-    {}
+    *ppvObject = nullptr;
 
-    HRESULT STDMETHODCALLTYPE QueryInterface(
-      REFIID riid,
-      void** ppvObject)
-    {
-      if (ppvObject == nullptr)
-        return E_POINTER;
-
-      *ppvObject = nullptr;
-
-      if (riid == __uuidof(IUnknown) ||
-          riid == __uuidof(IDirect3DVR9)) {
-        *ppvObject = ref(this);
-        return S_OK;
-      }
-
-      Logger::warn("D3D9VR::QueryInterface: Unknown interface query");
-      Logger::warn(str::format(riid));
-      return E_NOINTERFACE;
+    if (riid == __uuidof(IUnknown) || riid == __uuidof(IDirect3DVR9)) {
+      *ppvObject = ref(this);
+      return S_OK;
     }
 
-    HRESULT STDMETHODCALLTYPE GetVRDesc(
-      IDirect3DSurface9* pSurface,
-      D3D9_TEXTURE_VR_DESC* pDesc)
-    {
-      if (unlikely(pSurface == nullptr || pDesc == nullptr))
-        return D3DERR_INVALIDCALL;
+    Logger::warn("D3D9VR::QueryInterface: Unknown interface query");
+    Logger::warn(str::format(riid));
+    return E_NOINTERFACE;
+  }
 
-      D3D9Surface* surface = static_cast<D3D9Surface*>(pSurface);
+  HRESULT STDMETHODCALLTYPE GetVRDesc(IDirect3DSurface9* pSurface,
+                                      D3D9_TEXTURE_VR_DESC* pDesc)
+  {
+    if (unlikely(pSurface == nullptr || pDesc == nullptr))
+      return D3DERR_INVALIDCALL;
 
-      const auto* tex     = surface->GetCommonTexture();
+    D3D9Surface* surface = static_cast<D3D9Surface*>(pSurface);
 
-      const auto& desc    = tex->Desc();
-      const auto& image   = tex->GetImage();
-      const auto& device  = tex->Device()->GetDXVKDevice();
+    const auto* tex = surface->GetCommonTexture();
 
-      // I don't know why the image randomly is a uint64_t in OpenVR.
-      pDesc->Image            = uint64_t(image->handle());
-      pDesc->Device           = device->handle();
-      pDesc->PhysicalDevice   = device->adapter()->handle();
-      pDesc->Instance         = device->instance()->handle();
-      pDesc->Queue            = device->queues().graphics.queueHandle;
-      pDesc->QueueFamilyIndex = device->queues().graphics.queueIndex;
+    const auto& desc = tex->Desc();
+    const auto& image = tex->GetImage();
+    const auto& device = tex->Device()->GetDXVKDevice();
 
-      pDesc->Width            = desc->Width;
-      pDesc->Height           = desc->Height;
-      pDesc->Format           = tex->GetFormatMapping().FormatColor;
-      pDesc->SampleCount      = uint32_t(image->info().sampleCount);
+    // I don't know why the image randomly is a uint64_t in OpenVR.
+    pDesc->Image = uint64_t(image->handle());
+    pDesc->Device = device->handle();
+    pDesc->PhysicalDevice = device->adapter()->handle();
+    pDesc->Instance = device->instance()->handle();
+    pDesc->Queue = device->queues().graphics.queueHandle;
+    pDesc->QueueFamilyIndex = device->queues().graphics.queueIndex;
 
-      return D3D_OK;
-    }
+    pDesc->Width = desc->Width;
+    pDesc->Height = desc->Height;
+    pDesc->Format = tex->GetFormatMapping().FormatColor;
+    pDesc->SampleCount = uint32_t(image->info().sampleCount);
 
-    HRESULT STDMETHODCALLTYPE TransferSurface(
-      IDirect3DSurface9* pSurface,
-      BOOL waitResourceIdle)
-    {
-      if (unlikely(pSurface == nullptr))
-        return D3DERR_INVALIDCALL;
-      
-      auto* tex = static_cast<D3D9Surface*>(pSurface)->GetCommonTexture();
-      const auto& image = tex->GetImage();
+    return D3D_OK;
+  }
 
-      VkImageSubresourceRange subresources = {
-        VK_IMAGE_ASPECT_COLOR_BIT,
-        0, image->info().mipLevels,
-        0, image->info().numLayers
-      };
+  HRESULT STDMETHODCALLTYPE TransferSurface(IDirect3DSurface9* pSurface)
+  {
+    if (unlikely(pSurface == nullptr))
+      return D3DERR_INVALIDCALL;
 
-      m_device->TransformImage(
-        tex, &subresources,
-        image->info().layout,
-        VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL);
-      
-      // This wait may need to be on all Faces and Mip Levels (2 loops).
-      if (waitResourceIdle)
-        m_device->WaitForResource(image, tex->GetMappingBufferSequenceNumber(0u), D3DLOCK_READONLY);
+    auto* tex = static_cast<D3D9Surface*>(pSurface)->GetCommonTexture();
+    const auto& image = tex->GetImage();
 
-      return D3D_OK;
-    }
+    VkImageSubresourceRange subresources = { VK_IMAGE_ASPECT_COLOR_BIT,
+                                             0,
+                                             image->info().mipLevels,
+                                             0,
+                                             image->info().numLayers };
 
-    HRESULT STDMETHODCALLTYPE LockDevice()
-    { 
-      m_lock = m_device->LockDevice();
-      return D3D_OK;
-    }
+    m_device->TransformImage(tex,
+                             &subresources,
+                             image->info().layout,
+                             VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL);
 
-    HRESULT STDMETHODCALLTYPE UnlockDevice()
-    {
-      m_lock = D3D9DeviceLock();
-      return D3D_OK;
-    }
+    return D3D_OK;
+  }
 
-    HRESULT STDMETHODCALLTYPE WaitDeviceIdle()
-    {
-      m_device->Flush();
-      // Not clear if we need all here, perhaps...
-      m_device->SynchronizeCsThread(DxvkCsThread::SynchronizeAll);
-      m_device->GetDXVKDevice()->waitForIdle();
-      return D3D_OK;
-    }
+  HRESULT STDMETHODCALLTYPE LockDevice()
+  {
+    m_lock = m_device->LockDevice();
+    return D3D_OK;
+  }
 
-  private:
-    D3D9DeviceEx* m_device;
-    D3D9DeviceLock m_lock;
-  };
+  HRESULT STDMETHODCALLTYPE UnlockDevice()
+  {
+    m_lock = D3D9DeviceLock();
+    return D3D_OK;
+  }
+
+  HRESULT STDMETHODCALLTYPE WaitDeviceIdle()
+  {
+    m_device->Flush();
+    // Not clear if we need all here, perhaps...
+    m_device->SynchronizeCsThread(DxvkCsThread::SynchronizeAll);
+    m_device->GetDXVKDevice()->waitForIdle();
+    return D3D_OK;
+  }
+
+  HRESULT STDMETHODCALLTYPE BeginOVRSubmit() 
+  {
+    m_device->Flush();
+    m_device->SynchronizeCsThread(DxvkCsThread::SynchronizeAll);
+    m_device->GetDXVKDevice()->lockSubmission();
+
+    return D3D_OK;
+  }
+
+  HRESULT STDMETHODCALLTYPE EndOVRSubmit() 
+  {
+    m_device->GetDXVKDevice()->unlockSubmission();
+
+    return D3D_OK;
+  }
+
+private:
+  D3D9DeviceEx* m_device;
+  D3D9DeviceLock m_lock;
+};
 
 }
 
-HRESULT __stdcall Direct3DCreateVRImpl(IDirect3DDevice9* pDevice, IDirect3DVR9** pInterface) {
+HRESULT __stdcall Direct3DCreateVRImpl(IDirect3DDevice9* pDevice,
+                                       IDirect3DVR9** pInterface)
+{
   if (pInterface == nullptr)
     return D3DERR_INVALIDCALL;
 
